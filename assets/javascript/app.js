4b7c35f3ac0b9bc27c10a5db04b0a31f8bd08ba9
Azim Momin <azimmomin123@gmail.com>
2016-11-16 21:15:50 -0600

    Game functionality working! Css is now left and all other previous commit issues have been resolved.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 // Define number of seconds to answer questions
-var number = 6;
+var number = 3;
 var questionNumber = 0;
+var wrong = 0;
+var correct = 0;
+var unanswered = 0;
 
 // Question, Choices, Answers Array
 var questions = [{
@@ -16,107 +19,150 @@ var questions = [{
                 wrongGif:'http://i.giphy.com/iWREPosOrwzFm.gif',
                 time: 'http://i.giphy.com/l4hLT6kXPi9js7xFC.gif',
             	},{
-	            question : 'What is the name of the ship that crashed into an iceberg?',
+	            question : 'Who founded the game of basketball?',
 	            answers: [
-	            "Gigantic", "Titanic", "Monsterous", "Indestructible"
+	            "Dean Smith", 
+	            "John Wooden", 
+	            "Red Auerbach", 
+	            "James Naismith"
 	            ],
-	            rightAnswer: 1
+	            rightAnswer: 3,
+                rightGif:'http://i.giphy.com/l0HlRP71GpG8FOcMM.gif',
+                wrongGif:'http://i.giphy.com/iWREPosOrwzFm.gif',
+                time: 'http://i.giphy.com/l4hLT6kXPi9js7xFC.gif',
 	            }];
 
 //Variable that will hold the setInterval when we execute the run function
 var counter;
 //Clicking start will display the question and hide the button
-$("#start").click(displayQuestion);
 $("#start").click(function(){
+		displayQuestion();
+		//Hides the button after click
         $(this).hide();
     });
 
 // Displays the question and runs the timer after the start button is clicked. 
 function displayQuestion(){
-	run();
-	$(".questions").html("")
-	console.log(questionNumber);
-	$(".displayQuestion").html(questions[questionNumber].question);
-	// Displays each answer Choice
-	for(var i = 0; i < 4; i++){
-		var q = $("<button>");
-		q.addClass("choice");
-		// Gives each button a true of false depending on if its the correct answer.
-		if (questions[questionNumber].rightAnswer === i){
-			q.attr("value", true);
-		}else{
-			q.attr("value", false);
-		}
+	//Running this part of the if only if the there are enough questions for user input
+	var y = questionNumber + 1;
+	if (questions.length >= y){
+		run();
+			$(".content").html("");
+			//Displays the question in the console
+			console.log(questions[questionNumber].question);
+			//appends the question in to the content tag
+			$(".content").append(questions[questionNumber].question + "<br>");
+			// Displays each answer Choice
+			for(var i = 0; i < 4; i++){
+				//Button attribute given to each choice
+				var q = $("<button>");
+				//giving each button a choice class
+				q.addClass("choice");
+				// Gives each button a true of false depending on if its the correct answer.
+				if (questions[questionNumber].rightAnswer === i){
+					q.attr("value", true);
+				}else{
+					q.attr("value", false);
+				}
 
-		q.text(questions[questionNumber].answers[i]);
-	 	$(".questions").append(q);
+				q.text(questions[questionNumber].answers[i]);
+				//appends q with everything that is set above
+			 	$(".content").append(q);
+			}
 
-	 	//if ($(.q.attr("value")) == true)
+			//Givine the class "choice" a onclick function
+			$(".choice").on("click", function(){
+				var x = $(this).attr("value");
+				//Checking if true then calling appropriate display to the user
+				if (x === "true"){
+					console.log("right");
+					rightAnswer();
+				}else if (x === "false"){
+					console.log("wrong");
+					wrongAnswer();
+				}
+			});	
+	}else{
+		//gameOver when there are no more questions left
+		gameOver();
 	}
-
-	//Givine the class "choice" a onclick function
-	$(".choice").on("click", function(){
-		var x = $(this).attr("value");
-
-		if (x === "true"){
-			console.log("right");
-			rightAnswer();
-		}else if (x === "false"){
-			console.log("wrong");
-			wrongAnswer();
-		}
-	});	
+		
 }
 
 function wrongAnswer(){
 	stop();
 	//Removes the question and choices from page
-	$(".content").html("")
+	$(".content").html("");
 	//X is the array index for the right answer
 	var x = questions[questionNumber].rightAnswer;
 	//This will output the correct anwer and gif
 	$(".content").append("Sorry!!!! The correct answer is " + questions[questionNumber].answers[x] + "<br>");
 	var imageGif = $("<img>").attr("src", questions[questionNumber].wrongGif);
 	$(".content").append(imageGif);
-	//After selecting an answer we set it to the next question number
-	questionNumber++;
-	setTimeout(next,3000); //not working here
+	//Counter for wrong answers
+	wrong++;
+	//Display to the user the wrongAnswer screen for X seconds and 
+	//then calls next function to display next question
+	setTimeout(next,3000); 
 }
 
 function rightAnswer(){
 	stop();
 	//Removes the question and choices from page
-	$(".content").html("")
-	//X is the array index for the right answer
-	var x = questions[questionNumber].rightAnswer;
+	$(".content").html("");
 	//This will output the correct anwer and gif
 	$(".content").append("Correct!!! <br>");
 	var imageGif = $("<img>").attr("src", questions[questionNumber].rightGif);
 	$(".content").append(imageGif);
-	//After selecting an answer we set it to the next question number
-	questionNumber++;
-	setTimeout(next,3000); //not working here
+	//Counter for correct answers
+	correct++;
+	//Display to the user the wrongAnswer screen for X seconds and 
+	//then calls next function to display next question
+	setTimeout(next,3000); 
 }
 
 function outOfTime(){
 	stop();
 	//Removes the question and choices from page
-	$(".content").html("")
+	$(".content").html("");
 	//X is the array index for the right answer
 	var x = questions[questionNumber].rightAnswer;
 	//This will output the correct anwer and gif
 	$(".content").append("TOO SLOW, SLOW POKE!!!! The correct answer is " + questions[questionNumber].answers[x] + "<br>");
 	var imageGif = $("<img>").attr("src", questions[questionNumber].time);
 	$(".content").append(imageGif);
-	//After selecting an answer we set it to the next question number
-	questionNumber++;
-	setTimeout(next,3000); //not working here
-	
+	//Counter for unanswers
+	unanswered++;
+	//Display to the user the wrongAnswer screen for X seconds and 
+	//then calls next function to display next question
+	setTimeout(next,3000); 
 }
 
 //Timer for next question
 function next(){
+	number = 3;
+	//After selecting an answer we set it to the next question number
+	questionNumber++;
 	displayQuestion();
+}
+
+//GameOver
+function gameOver(){
+	stop();
+	$("#timer").html("");
+	$(".content").html("Game Over" + "<br>");
+	$(".content").append("Correct Answers: " + correct + "<br>");
+	$(".content").append("Incorrect Answers: " + wrong + "<br>");
+	$(".content").append("Unanswered: " + unanswered + "<br>");
+
+	$("#start").show();
+	
+	// Redefine number of seconds to answer questions
+	number = 3;
+	questionNumber = 0;
+	wrong = 0;
+	correct = 0;
+	unanswered = 0;       
 }
 
 // Starts the Timer
@@ -130,18 +176,12 @@ function decrement(){
 	$("#timer").html("<h2>Time Remaining: " + number + "</h2>");
 
 	//If timer runs out then switch to the next question
-	if (number === 0){
+	if (number <= 0){
 		outOfTime();
-		questionNumber++;
-		number = 6;	
-		
 	}
 }
 
 // Prevents thh timer from going past 0.
 function stop(){
 	clearInterval(counter);
-	//Empty the "questions" div.
-    //$(".body").hide();
-    //correctAnswers();
 }
